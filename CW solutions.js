a0744112775f505d98a6be3ff7d730341c194dd9
egory4fash <egormaksul@gmail.com>
2021-10-05 09:47:24 +0300

    Update CW solutions.js

--- CW solutions.js
+++ CW solutions.js
@@ -15,3 +15,11 @@ function sumMix(x){
   }
 return sum;
 }
+
+function squareSum(numbers){
+let sum = 0;
+  for (let i=0;i<numbers.length;i++) {
+    sum+=numbers[i]*numbers[i];
+  }
+  return sum
+}
