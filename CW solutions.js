72b3f35c6dc8303f25c2bf4255d2ac42270b7938
egory4fash <egormaksul@gmail.com>
2021-10-05 09:24:40 +0300

    Update CW solutions.js

--- CW solutions.js
+++ CW solutions.js
@@ -7,3 +7,11 @@ function noSpace(x){
 str = x.replace(/\s/g, '');
   return str;
 }
+
+function sumMix(x){
+  let sum = 0;
+  for (let i = 0;i<x.length;i++) {
+    sum +=+(x[i]);
+  }
+return sum;
+}
