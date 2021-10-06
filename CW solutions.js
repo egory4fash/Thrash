c40463a6e8aa97cea1814bc3fa95f0c5f45e60c7
egory4fash <egormaksul@gmail.com>
2021-10-06 19:53:35 +0300

    beasts

--- CW solutions.js
+++ CW solutions.js
@@ -148,3 +148,7 @@ function ensureQuestion(s) {
     return newStr;
   }
 }
+
+function feast(beast, dish) {
+return (beast[0] == dish[0]&&beast[beast.length-1] == dish[dish.length-1]) ? true : false;
+}
