c7fa0dd78dec911f0dde919a74f9d7790679f04a
egory4fash <egormaksul@gmail.com>
2021-10-06 14:43:51 +0300

    letter in str

--- CW solutions.js
+++ CW solutions.js
@@ -89,3 +89,13 @@ function hello(name) {
     return "Hello, World!";
   }
 }
+
+function strCount(str, letter){  
+  let cnt = 0;
+  for (let i = 0;i<str.length;i++) {
+    if (str[i]==letter) {
+      cnt++;
+    }
+  }
+  return cnt;
+}
